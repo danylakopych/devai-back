43c15bb0a90469f29f10e5dfc632a3ba162f39c5
Danylo Kopych <danylo.kopych@devops.ncia.nato.int>
2024-09-23 11:06:35 +0300

    create services for models

--- src/document/document.service.ts
+++ src/document/document.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { Prisma, Document } from '@prisma/client';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  constructor(private prisma: PrismaService) {}
+
+  async createDocument(data: Prisma.DocumentCreateInput): Promise<Document> {
+    return this.prisma.document.create({
+      data,
+    });
+  }
+
+  async getDocumentById(id: number): Promise<Document | null> {
+    return this.prisma.document.findUnique({
+      where: { id },
+    });
+  }
+
+  async getAllDocuments(): Promise<Document[]> {
+    return this.prisma.document.findMany();
+  }
+
+  async updateDocument(
+    id: number,
+    data: Prisma.DocumentUpdateInput
+  ): Promise<Document> {
+    return this.prisma.document.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteDocument(id: number): Promise<Document> {
+    return this.prisma.document.delete({
+      where: { id },
+    });
+  }
+}
